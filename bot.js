c1b084edf11c41d821840fb89a4294ad264e2a9e
CaptainLeviathan <levmckinney@gmail.com>
2017-04-22 00:53:05 -0700

    some more work on getting this to work

--- bot.js
+++ bot.js
@@ -1,42 +1,52 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const operator = "~";
+const operator = '~';
 
-function functionName(fun)
-{
-  var ret = fun.toString();
-  ret = ret.substr('function '.length);
-  ret = ret.substr(0, ret.indexOf('('));
-  ret = ret.toString();
-  return ret;
+var root = {};
+
+function BaseCommand (msg) {
+  root.commands.hiThere = function (msg, layer) {
+    return 'hi there' + msg.obj.author;
+  };
+
+  root.commands.help = function (msg, layer) {
+    return 'shit out of luck';
+  };
+
+  root.defalt = function () {
+    return 'NoCammand say some thing';
+  };
+
+  root.error = function () {
+    return 'NoCammand say some thing';
+  };
+  nxtLayerTree(msg, root, 0);
 }
 
-function isCommand(msg)
-{
+function isCommand (msg) {
    return msg.content.startsWith(operator);
 }
 
-function brakeUp(msg)
-{
+function parseCammands (msg) {
   return {
-      'commands' : msg.content.toLowerCase().slice(1).split(" "),
+      'commands': msg.content.toLowerCase().slice(1).split(' '),
       'obj' : msg
   };
 }
 
-function caller(msg,c,layer)
+function nxtLayerTree (msg, base, layer)
 {
   if(msg.commands.length - 1 < layer)
   {
-    return c.defalt();
+    return base.defalt();
   }
   try
   {
-    return c.commands[msg.commands[i]](msg, layer + 1);
+    return base.commands[msg.commands[layer]](msg, layer + 1);
   }
   catch (e)
   {
-    c.error();
+    base.error();
   }
 }
 
@@ -48,33 +58,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (isCommand(message))
     {
-          message.reply(root(brakeUp(message)));
+          message.reply(BaseCommand(parseCammands(message)));
     }
 });
 
-
-function root (msg) {
-  let c = {};
-
-  return caller(msg, c, 0);
-
-  c.commands.hiThere = function (msg, layer) {
-    return 'hi there' + msg.obj.author;
-  };
-
-  c.commands.help = function (msg, layer) {
-    return 'shit out of luck';
-  };
-
-  c.defalt = function noCammand () {
-    return 'NoCammand say some thing';
-  };
-
-  c.error = function () {
-    return 'NoCammand say some thing';
-  };
-}
-
-
-
-client.login('MzA1MTU4MDcxNTg2NTIxMDk4.C9xYTA.VK0hUjgPlBzXFs5ujNoHkUd_XSg');
+client.login('token');
